e5ec63fc055ee15269c5a265aec0df5b652f3d76
srinivasxtg <srinu.y146@gmail.com>
2017-08-18 19:10:45 +0530

    Add files via upload

---
+++ SampleJavaForIntruduction.java
@@ -0,0 +1,17 @@
+
+public class SampleJavaForIntruduction {
+	
+	
+	void function1(){
+		
+		
+	}
+	
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Hello world");
+		
+	}
+
+}
